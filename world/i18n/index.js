8c7ea8f2991ad474f03e525760415c330486eb37
wangyong1997 <1044656020@qq.com>
2025-08-07 14:41:47 +0800

    地图json 数据

---
+++ world/i18n/index.js
@@ -0,0 +1,9 @@
+export default {
+    zh: {
+        'Central Singapore Community Development Council': '新加坡中央社区发展委员会',
+        'South West Community Development Council': '西南社区发展局',
+        'North West Community Development Council': '西北社区发展局',
+        'South East Community Development Council': '东南社区发展委员会',
+        'North East Community Development Council': '东北社区发展局'
+    }
+}
